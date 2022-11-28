36722e22b0d52addb64f25acb935e5209f834603
ibrahimAlphaSquad <mibrahim.alphasquad@gmail.com>
2022-11-28 13:21:24 +0500

    Changes

--- App.js
+++ App.js
@@ -6,7 +6,7 @@
  * @flow strict-local
  */
 
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import type { Node } from 'react';
 import {
   Alert,
@@ -31,6 +31,8 @@ import {
   LearnMoreLinks,
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
+
+import Canvas, { Image as CanvasImage } from 'react-native-canvas';
 
 /* $FlowFixMe[missing-local-annot] The type annotation(s) required by Flow's
  * LTI update could not be added via codemod */
@@ -62,6 +64,9 @@ const Section = ({ children, title }): Node => {
 
 const App: () => Node = () => {
   const isDarkMode = useColorScheme() === 'dark';
+  const [canvasD, setCanvas] = useState(false);
+  const [shufflePzl, setShuffle] = useState(false);
+  const ref = useRef(false);
 
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
@@ -75,19 +80,6 @@ const App: () => Node = () => {
   // Timer
   const [time, setTime] = useState(0);
   const [running, setRunning] = useState(false);
-
-  useEffect(() => {
-    let interval;
-    if (running) {
-      interval = setInterval(() => {
-        setTime((prevTime) => prevTime + 10);
-      }, 10);
-    } else if (!running) {
-      clearInterval(interval);
-    }
-    // console.log(Math.floor((time / 60000) % 60))
-    return () => clearInterval(interval);
-  }, [running]);
 
 
   function setRandomImage() {
@@ -120,11 +112,356 @@ const App: () => Node = () => {
   }
 
   const handleCreatePuzzle = () => {
-    console.log("puzzleDifficulty",puzzleDifficulty)
+    console.log("puzzleDifficulty", puzzleDifficulty)
     if (running !== true) {
       setRunning(true);
-    }4
+      setShuffle(true);
+    }
   }
+
+  const handleCanvas = canvas => {
+    const PUZZLE_HOVER_TINT = "#6ec5eb";
+    const img = new CanvasImage(canvas);
+    const stage = canvas.getContext("2d");
+    let difficulty = puzzleDifficulty;
+    let pieces;
+    let puzzleWidth;
+    let puzzleHeight;
+    let pieceWidth;
+    let pieceHeight;
+    let currentPiece;
+    let currentDropPiece;
+    let mouse;
+    img.addEventListener("load", onImage, false);
+
+    canvas.width = 300;
+    canvas.height = 300;
+    const context = canvas.getContext('2d');
+
+    img.addEventListener('load', () => {
+      context.drawImage(img, 0, 0, 100, 100);
+    });
+
+    img.addEventListener('load', onImage, true);
+    img.src = puzzleSource;
+
+    function initPuzzle() {
+      pieces = [];
+      mouse = {
+        x: 0,
+        y: 0,
+      };
+      currentPiece = null;
+      currentDropPiece = null;
+      stage.drawImage(
+        img,
+        0,
+        0,
+        puzzleWidth,
+        puzzleHeight,
+        0,
+        0,
+        puzzleWidth,
+        puzzleHeight,
+      );
+      // createTitle("Click to Start Puzzle");
+
+      buildPieces();
+    }
+    //canvas initial rebder
+    // function setCanvas() {
+    // canvas.width = puzzleWidth;
+    // canvas.height = puzzleHeight;
+    // canvas.style.border = '1px solid black';
+    // stage.drawImage(
+    //     img,
+    //     0,
+    //     0,
+    //     puzzleWidth,
+    //     puzzleHeight,
+    //     0,
+    //     0,
+    //     puzzleWidth,
+    //     puzzleHeight
+    // );
+    // }
+    //image load handler based on grid size
+    function onImage() {
+      pieceWidth = Math.floor(canvas.width / difficulty);
+      pieceHeight = Math.floor(canvas.height / difficulty);
+      puzzleWidth = pieceWidth * difficulty;
+      puzzleHeight = pieceHeight * difficulty;
+      // setCanvas();
+      initPuzzle();
+    }
+    //create pieces of tiles
+    function buildPieces() {
+      let i;
+      let piece;
+      let xPos = 0;
+      let yPos = 0;
+      for (i = 0; i < difficulty * difficulty; i++) {
+        piece = {};
+        piece.sx = xPos;
+        piece.sy = yPos;
+        pieces.push(piece);
+        xPos += pieceWidth;
+        if (xPos >= puzzleWidth) {
+          xPos = 0;
+          yPos += pieceHeight;
+        }
+      }
+
+      shufflePzl && shufflePuzzle();
+    }
+    //shuffle Grid pieces
+    function shufflePuzzle() {
+      pieces = shuffleArray(pieces);
+      stage.clearRect(0, 0, puzzleWidth, puzzleHeight);
+      let xPos = 0;
+      let yPos = 0;
+      for (const piece of pieces) {
+        piece.xPos = xPos;
+        piece.yPos = yPos;
+        stage.drawImage(
+          img,
+          piece.sx,
+          piece.sy,
+          pieceWidth,
+          pieceHeight,
+          xPos,
+          yPos,
+          pieceWidth,
+          pieceHeight,
+        );
+        stage.strokeRect(xPos, yPos, pieceWidth, pieceHeight);
+        xPos += pieceWidth;
+        if (xPos >= puzzleWidth) {
+          xPos = 0;
+          yPos += pieceHeight;
+        }
+      }
+      onPuzzleClick();
+    }
+    //check the piece is draged or not
+    function checkPieceClicked() {
+      for (const piece of pieces) {
+        if (
+          mouse.x < piece.xPos ||
+          mouse.x > piece.xPos + pieceWidth ||
+          mouse.y < piece.yPos ||
+          mouse.y > piece.yPos + pieceHeight
+        ) {
+          //PIECE NOT HIT
+        } else {
+          return piece;
+        }
+      }
+      return null;
+    }
+    //on refresh update puzzle
+    function updatePuzzle(e) {
+      console.log("first")
+      currentDropPiece = null;
+      if (e?.clientX || e?.clientX == 0) {
+        mouse.x = e?.clientX - canvas.offsetLeft;
+        mouse.y = e?.clientY - canvas.offsetTop;
+      } else if (e?.offsetX || e?.offsetX == 0) {
+        mouse.x = e?.offsetX - canvas.offsetLeft;
+        mouse.y = e?.offsetY - canvas.offsetTop;
+      }
+      stage.clearRect(0, 0, puzzleWidth, puzzleHeight);
+      for (const piece of pieces) {
+        if (piece == currentPiece) {
+          continue;
+        }
+        stage.drawImage(
+          img,
+          piece.sx,
+          piece.sy,
+          pieceWidth,
+          pieceHeight,
+          piece.xPos,
+          piece.yPos,
+          pieceWidth,
+          pieceHeight,
+        );
+        stage.strokeRect(piece.xPos, piece.yPos, pieceWidth, pieceHeight);
+        if (currentDropPiece == null) {
+          if (
+            mouse.x < piece.xPos ||
+            mouse.x > piece.xPos + pieceWidth ||
+            mouse.y < piece.yPos ||
+            mouse.y > piece.yPos + pieceHeight
+          ) {
+            //NOT OVER
+          } else {
+            currentDropPiece = piece;
+            stage.save();
+            stage.globalAlpha = 0.4;
+            stage.fillStyle = PUZZLE_HOVER_TINT;
+            stage.fillRect(
+              currentDropPiece.xPos,
+              currentDropPiece.yPos,
+              pieceWidth,
+              pieceHeight,
+            );
+            stage.restore();
+          }
+        }
+      }
+      stage.save();
+      stage.globalAlpha = 0.6;
+      stage.drawImage(
+        img,
+        currentPiece.sx,
+        currentPiece.sy,
+        pieceWidth,
+        pieceHeight,
+        mouse.x - pieceWidth / 2,
+        mouse.y - pieceHeight / 2,
+        pieceWidth,
+        pieceHeight,
+      );
+      stage.restore();
+      stage.strokeRect(
+        mouse.x - pieceWidth / 2,
+        mouse.y - pieceHeight / 2,
+        pieceWidth,
+        pieceHeight,
+      );
+    }
+    //Drag puzzle
+    function onPuzzleClick(e) {
+      if (e?.clientX || e?.clientX === 0) {
+        mouse.x = e?.clientX - canvas.offsetLeft;
+        mouse.y = e?.clientY - canvas.offsetTop;
+      }
+      // else if (e.offsetX || e.offsetX === 0) {
+      //     mouse.x = e.offsetX - canvas.offsetLeft;
+      //     mouse.y = e.offsetY - canvas.offsetTop;
+      // }
+      currentPiece = checkPieceClicked();
+      if (currentPiece !== null) {
+        stage.clearRect(
+          currentPiece.xPos,
+          currentPiece.yPos,
+          pieceWidth,
+          pieceHeight,
+        );
+        stage.save();
+        stage.globalAlpha = 0.9;
+        stage.drawImage(
+          img,
+          currentPiece.sx,
+          currentPiece.sy,
+          pieceWidth,
+          pieceHeight,
+          mouse.x - pieceWidth / 2,
+          mouse.y - pieceHeight / 2,
+          pieceWidth,
+          pieceHeight,
+        );
+        stage.restore();
+        // document.onpointermove = updatePuzzle;
+        // document.onpointerup = pieceDropped;
+        updatePuzzle();
+        pieceDropped();
+      }
+    }
+    //game over
+    function gameOver() {
+      // document.onpointerdown = null;
+      // document.onpointermove = null;
+      // document.onpointerup = null;
+
+      initPuzzle();
+    }
+    //drop puzzle
+    function pieceDropped(e) {
+      // document.onpointermove = null;
+      // document.onpointerup = null;
+
+      if (currentDropPiece !== null) {
+        let tmp = {
+          xPos: currentPiece.xPos,
+          yPos: currentPiece.yPos,
+        };
+        currentPiece.xPos = currentDropPiece.xPos;
+        currentPiece.yPos = currentDropPiece.yPos;
+        currentDropPiece.xPos = tmp.xPos;
+        currentDropPiece.yPos = tmp.yPos;
+      }
+      resetPuzzleAndCheckWin();
+    }
+    //Reset when done
+    function resetPuzzleAndCheckWin() {
+      stage.clearRect(0, 0, puzzleWidth, puzzleHeight);
+      let gameWin = true;
+      for (let piece of pieces) {
+        stage.drawImage(
+          img,
+          piece.sx,
+          piece.sy,
+          pieceWidth,
+          pieceHeight,
+          piece.xPos,
+          piece.yPos,
+          pieceWidth,
+          pieceHeight,
+        );
+        stage.strokeRect(piece.xPos, piece.yPos, pieceWidth, pieceHeight);
+        if (piece.xPos != piece.sx || piece.yPos != piece.sy) {
+          gameWin = false;
+        }
+      }
+      if (gameWin) {
+        // setToaster(true);
+        // console.log("Welcome to the AK's Team --->");
+        // createTitle("Welcome to the AK's Team");
+        // setModalState(true);
+        // setWelcomeConfitte(true);
+        // setIsActive(false);
+        setTimeout(gameOver, 200);
+        // setSeconds(pre => localStorage.setItem('seconds', JSON.stringify(pre)));
+      }
+    }
+
+    function shuffleArray(o) {
+      for (
+        var j, x, i = o.length;
+        i;
+        j = parseInt(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x
+      );
+      return o;
+    }
+    // Grid size
+    function updateDifficulty(e) {
+      difficulty = e;
+      pieceWidth = window.matchMedia('(max-width: 768px)').matches
+        ? Math.floor(370 / difficulty)
+        : Math.floor(img.width / difficulty);
+      pieceHeight = window.matchMedia('(max-width: 768px)').matches
+        ? Math.floor(300 / difficulty)
+        : Math.floor(img.width / difficulty);
+      puzzleWidth = pieceWidth * difficulty;
+      puzzleHeight = pieceHeight * difficulty;
+      gameOver();
+    }
+    // document.querySelector('#GridSize').oninput = updateDifficulty;
+  };
+
+
+  useEffect(() => {
+    if (puzzleDifficulty >= 2 || puzzleDifficulty !== '') {
+      setCanvas(false);
+      handleCanvas(ref.current);
+    } else {
+      setShuffle(false);
+      setCanvas(true);
+      console.log("first")
+    }
+  }, [ref, puzzleDifficulty, shufflePzl]);
 
   return (
     <ScrollView style={backgroundStyle}>
@@ -141,6 +478,7 @@ const App: () => Node = () => {
           <TextInput
             style={styles.inputContainer}
             placeholder="Enter Puzzle Size"
+            keyboardType='numeric'
             onChangeText={(text) => {
               setPuzzleDifficulty(text);
               setpuzzleSource(setRandomImage());
@@ -150,10 +488,20 @@ const App: () => Node = () => {
           />
         </View>
         <TouchableWithoutFeedback
-          onPress={() => { handleCreatePuzzle() }}>
+          onPress={() => {
+            if (running !== true) {
+              setRunning(true);
+              setShuffle(true);
+            }
+          }}>
           <Text style={styles.button}>Create Puzzle</Text>
         </TouchableWithoutFeedback>
-        {/* <Canvas */}
+        <View style={styles.canvasView}>
+          <Canvas
+            ref={ref}
+            style={[styles.Canvas, canvasD && { display: 'none' }]}
+          />
+        </View>
       </SafeAreaView>
     </ScrollView >
   );
@@ -180,7 +528,10 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignContent: 'center',
-
+  },
+  canvasView: {
+    marginLeft: 40,
+    marginTop: 20
   },
   title: {
     fontSize: 30,
